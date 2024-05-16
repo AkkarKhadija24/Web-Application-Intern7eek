96c40c152422e60af2c7d07e43912745077f9048
AkkarKhadija24 <khadijakkarah@gmail.com>
2024-05-16 22:12:20 +0200

    Login-Done

--- Intern7eek/src/Model/Facade.java
+++ Intern7eek/src/Model/Facade.java
@@ -1,8 +1,12 @@
 package Model;
 import BD.*;
+
+import java.util.List;
+
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,23 +26,33 @@ public class Facade {
     @Produces({ "application/json" })
     public Response login(LoginRequest loginRequest) {
         System.out.println("coucou");
+        
         // Logique de validation des identifiants
-        // Pour l'instant, renvoyons une réponse de succès pour démonstration
-        if ("test@example.com".equals(loginRequest.getEmail()) && "password".equals(loginRequest.getPassword())) {
-            return Response.ok().entity("{\"message\": \"Utilisateur connecté avec succès\"}").build();
+        String mail = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        System.out.println("mail : " + mail + " et mdp : " + password);
+        List<User> employeur = em.createQuery("SELECT u FROM User u WHERE u.mail = :mail AND u.password = :password", User.class)
+                                 .setParameter("mail", mail)
+                                 .setParameter("password", password)
+                                 .getResultList();
+
+        if (!employeur.isEmpty() && employeur.size() == 1) {
+            User user = employeur.get(0); // retourne le premier (et seul normalement) utilisateur trouvé
+            // Retourne une réponse de succès avec les détails de l'utilisateur
+            return Response.ok().entity(user).build();
         } else {
+            // Retourne une réponse non autorisée si aucun utilisateur trouvé
             return Response.status(Response.Status.UNAUTHORIZED).entity("Identifiants incorrects").build();
         }
     }
 }
-
 // Classe pour représenter la requête de connexion
 class LoginRequest {
     private String email;
     private String password;
 
     // Getters et setters
-    public String getEmail() {back-end
+    public String getEmail() {
         return email;
     }
 
